e9883d8cf714114be54bfc4f9c79c47916f5d9a1
Sergey Volodin <s.volodin@corp.mail.ru>
2022-04-04 20:26:02 +0300

    fix divideAndConquer approach in merge-k-sorted-lists

--- merge-k-sorted-lists/divideAndConquer.ts
+++ merge-k-sorted-lists/divideAndConquer.ts
@@ -4,25 +4,20 @@ import mergeTwoLists from '../merge-two-sorted-lists/compare1by1';
 // time O(N*log(k))
 // space O(1)
 function mergeKLists(lists: Array<ListNode | null>): any {
-	for (let N = lists.length; N > 1; N = Math.ceil(N / 2)) {
-		// iterate each 2
-		let listsIndex = 0;
-		for (let i = 0; i < N; i += 2) {
-			if (!lists[i + 1]) {
-				lists[listsIndex++] = lists[i];
-				continue;
-			}
+	let interval = 1;
 
-			if (!lists[i]) {
-				lists[listsIndex++] = lists[i + 1];
-				continue;
-			}
-
-			lists[listsIndex++] = mergeTwoLists(lists[i], lists[i + 1]);
-		}
+	if (!lists.length) {
+		return null;
 	}
 
-	return lists[0] || null;
+	while (interval < lists.length) {
+		for (let i = 0; i < lists.length - interval; i += interval * 2) {
+			lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+		}
+		interval *= 2;
+	}
+
+	return lists[0];
 }
 
 export default mergeKLists;
